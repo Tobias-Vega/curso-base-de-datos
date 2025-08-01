85cb89ddb20e4361f19143959e3b752447d5774b
Tobias <tobiasvega1210@gmail.com>
2025-07-31 21:23:10 -0300

    clausula union mejorada

--- 21-union.sql
+++ 21-union.sql
@@ -1,3 +1,8 @@
 SELECT columna1, columna2 FROM tabla1
 UNION
 SELECT columna1, columna2 FROM tabla2;
+
+-- incluir duplicados
+SELECT columna1, columna2 FROM tabla1
+UNION ALL
+SELECT columna1, columna2 FROM tabla2;
